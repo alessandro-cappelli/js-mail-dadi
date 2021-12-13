0c1e909a9ae648075871a5fab9f97118332d3481
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-13 16:27:55 +0100

    email

---
+++ Mail/myscript.js
@@ -0,0 +1,19 @@
+let email = ["luca@gmail.it", "ale@gmail.it", "andrea@gmail.it"];
+
+let insertEmail = prompt('inserisci la tua emai');
+let controllo = false;
+
+for(let index = 0; index < email.length; index++){
+    if(insertEmail == email[index]){
+        controllo = true
+    }
+    console.log(email[index], controllo);
+}
+
+if(controllo){
+    console.log('EMAIL GIUSTA')
+    document.getElementById('email-inserita') .innerHTML = "Benvenuto!"
+}else{
+    console.log('EMAIL NON TROVATA')
+    document.getElementById('email-inserita') .innerHTML = "Account non trovato"
+}
